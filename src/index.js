76f33010b06daf6cccff1fd40bb9039ed55e8d0b
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-07-19 14:40:25 +0200

    Create a show list function and add focus on date input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-// "use strict";
+"use strict";
 
 const themeBtn = document.querySelector(".themeBtn");
+const inputDate = document.querySelectorAll(".inputDate");
+const select = document.querySelector(".select");
 
 let theme = localStorage.getItem("theme");
 if (theme === "dark") document.body.classList.add("dark");
 else document.body.classList.add("light");
 
-//Funkcja zmienijąca motyw na stronie
+//change theme on a page
 themeBtn.addEventListener("click", () => {
   console.log("click");
   if (theme === "dark") {
@@ -19,4 +21,48 @@ themeBtn.addEventListener("click", () => {
     theme = "dark";
   }
   localStorage.setItem("theme", theme);
+});
+
+//focus on date type input
+inputDate.forEach((input) =>
+  input.addEventListener("change", () => {
+    input.classList.add("inputDate--focus");
+  })
+);
+
+// show select list
+select.addEventListener("click", () => {
+  const selectList = document.querySelector(".select__list");
+  const selectListItem = document.querySelector(".select__listItem");
+  selectList.classList.toggle("select__list--active");
+
+  selectListItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      select.textContent = item.textContent;
+    });
+  });
+  selectListItem.forEach((item) => {
+    item.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        select.textContent = item.textContent;
+      }
+    });
+  });
+  window.addEventListener("click", (e) => {
+    closeList(e);
+  });
+  window.addEventListener("touch", (e) => {
+    closeList(e);
+  });
+  select.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") closeList();
+  });
+
+  // hide select list
+  function closeList(e) {
+    console.log(e);
+    if (!e.target.classList.contains("select__list")) {
+      selectList.classList.remove("select__list--active");
+    }
+  }
 });
