8b217fc59d1c1cccd6a5d77267e3c19c5c5c66b3
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-07-25 22:16:30 +0200

    Modified a checkInputsNumber.js

--- src/js/checkInputNumber.js
+++ src/js/checkInputNumber.js
@@ -1,6 +1,6 @@
 export function checkInputNumber(e) {
   if (e.target.value < 1) {
-    e.target.value = 1;
+    e.target.value = "";
   } else {
     if (e.target.name === "age") {
       if (e.target.value > 199) e.target.value = 199;
